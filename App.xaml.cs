21a8ebdf4cb29437ab2ad25aad22c26c985fc0eb
Wei LI <liwei.bfm@outlook.com>
2023-03-30 14:49:10 +0800

    修正项目名称。

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace FocasSimple2
+namespace FocasSimple
 {
     /// <summary>
     /// App.xaml 的交互逻辑
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FocasSimple2
+namespace FocasSimple
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
